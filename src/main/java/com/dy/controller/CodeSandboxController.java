e8cdb8ff4d43c528c471e966ed28b1079f139f20
dy <dyu32345@gmail.com>
2024-07-30 18:55:41 +0800

    代码沙箱实现简单鉴权

--- src/main/java/com/dy/controller/CodeSandboxController.java
+++ src/main/java/com/dy/controller/CodeSandboxController.java
@@ -4,10 +4,14 @@ import com.dy.JavaNativeCodeSandbox;
 import com.dy.model.ExecuteCodeRequest;
 import com.dy.model.ExecuteCodeResponse;
 import jakarta.annotation.Resource;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.http.HttpRequest;
 
 /**
  * @Author: dy
@@ -18,11 +22,24 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class CodeSandboxController {
 
+    //  定义鉴权请求头
+    public static final String AUTH_REQUEST_HEADER = "auth";
+    public static final String AUTH_REQUEST_SECRET = "dingyu";
+
     @Resource
     private JavaNativeCodeSandbox javaNativeCodeSandbox;
 
     @PostMapping("/executeCode")
-    public ExecuteCodeResponse executeCode(@RequestBody ExecuteCodeRequest executeCodeRequest) {
+    public ExecuteCodeResponse executeCode(@RequestBody ExecuteCodeRequest executeCodeRequest, HttpServletRequest request,
+                                           HttpServletResponse response) {
+        //  获取请求头
+        String authHeader = request.getHeader(AUTH_REQUEST_HEADER);
+        if (!AUTH_REQUEST_SECRET.equals(authHeader)) {
+            response.setStatus(403);
+            return null;
+        }
+
+
         log.info("远程代码沙箱调用: {}", executeCodeRequest);
         return javaNativeCodeSandbox.executeCode(executeCodeRequest);
     }
